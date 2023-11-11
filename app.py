4978c622c4f841a523cd1f44fea2b35ed510b67c
IzamaraJordao <izamara.jordao@mazzafc.tech>
2023-11-11 10:25:39 -0300

    add crud pacientes

--- app.py
+++ app.py
@@ -76,6 +76,79 @@ def delete_user(id):
 
 
 
+class Paciente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idade = db.Column(db.Integer)
+    sexo = db.Column(db.String(10))
+    tipo_dor_peito = db.Column(db.String(50))
+    pressao_arterial = db.Column(db.String(20))
+    colesterol_serico = db.Column(db.Integer)
+    acucar_jejum = db.Column(db.Integer)
+    resultado_eletrocardiograma = db.Column(db.String(50))
+    freq_cardiaca_max = db.Column(db.Integer)
+    dor_peito_pratica_atividade = db.Column(db.Boolean)
+    pico_antigo = db.Column(db.String(50))
+    aceleracao_batimentos = db.Column(db.Boolean)
+    avaliacao_exclusao = db.Column(db.String(50))
+    condicao_pre_existente = db.Column(db.Boolean)
 
+    def to_json(self):
+        return {
+            "id": self.id,
+            "idade": self.idade,
+            "sexo": self.sexo,
+            "tipo_dor_peito": self.tipo_dor_peito,
+            "pressao_arterial": self.pressao_arterial,
+            "colesterol_serico": self.colesterol_serico,
+            "acucar_jejum": self.acucar_jejum,
+            "resultado_eletrocardiograma": self.resultado_eletrocardiograma,
+            "freq_cardiaca_max": self.freq_cardiaca_max,
+            "dor_peito_pratica_atividade": self.dor_peito_pratica_atividade,
+            "pico_antigo": self.pico_antigo,
+            "aceleracao_batimentos": self.aceleracao_batimentos,
+            "avaliacao_exclusao": self.avaliacao_exclusao,
+            "condicao_pre_existente": self.condicao_pre_existente
+        }
+
+# Rota para criar um novo paciente
+@app.route('/pacientes', methods=['POST'])
+def add_paciente():
+    data = request.get_json()
+    novo_paciente = Paciente(**data)
+    db.session.add(novo_paciente)
+    db.session.commit()
+    return jsonify({"message": "Paciente adicionado com sucesso!"})
+
+# Rota para listar todos os pacientes
+@app.route('/pacientes', methods=['GET'])
+def get_pacientes():
+    pacientes = Paciente.query.all()
+    pacientes_json = [paciente.to_json() for paciente in pacientes]
+    return jsonify(pacientes_json)
+
+# Rota para obter detalhes de um paciente específico
+@app.route('/pacientes/<int:paciente_id>', methods=['GET'])
+def get_paciente(paciente_id):
+    paciente = Paciente.query.get_or_404(paciente_id)
+    return jsonify(paciente.to_json())
+
+# Rota para atualizar os dados de um paciente
+@app.route('/pacientes/<int:paciente_id>', methods=['PUT'])
+def update_paciente(paciente_id):
+    paciente = Paciente.query.get_or_404(paciente_id)
+    data = request.get_json()
+    for key, value in data.items():
+        setattr(paciente, key, value)
+    db.session.commit()
+    return jsonify({"message": "Dados do paciente atualizados com sucesso!"})
+
+# Rota para excluir um paciente
+@app.route('/pacientes/<int:paciente_id>', methods=['DELETE'])
+def delete_paciente(paciente_id):
+    paciente = Paciente.query.get_or_404(paciente_id)
+    db.session.delete(paciente)
+    db.session.commit()
+    return jsonify({"message": "Paciente excluído com sucesso!"})
+    
 if __name__ == '__main__':
     app.run()
